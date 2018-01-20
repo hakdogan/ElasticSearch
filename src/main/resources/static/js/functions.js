e310105c172b7fdf418268ff5ab6d79e84eccfd0
Hüseyin Akdoğan <hakdogan75@gmail.com>
2018-01-20 16:44:26 +0300

    refactor function.js

--- src/main/resources/static/js/functions.js
+++ src/main/resources/static/js/functions.js
@@ -1,24 +1,21 @@
 function fillTable(){
 
-    $('#myTable').find("tr:gt(0)").remove();
+    $("#myTable").find("tr:gt(0)").remove();
     $.ajax({
-        type: 'GET',
+        type: "GET",
         url: "/api/all",
         contentType: "application/json; charset=utf-8",
         success: function(result) {
-
-            for (i = 0; i <= result.length - 1; i++) {
-                var obj = result[i];
-                $('#myTableBody').append('<tr><td>' + obj.firstname + '</td>'
-                    + '<td>' + obj.lastname + '</td>'
-                    + '<td>' + obj.message + '</td>'
-                    + '<td><button type=\'button\' onclick=\"fillFields(\'' + obj.id + '\', \'' + obj.firstname + '\', \''
-                    + obj.lastname + '\', \'' + obj.message + '\')\" class=\'btn btn-light\'>Update</button>'
-                    + ' | <button type=\'button\' onclick=deleteDocument(\'' + obj.id + '\') class=\'btn btn-danger\'>Delete</button></td>');
-            };
+            result.forEach(function (obj) {
+                $("#myTableBody").append("<tr><td>" + obj.firstname + "</td>"
+                    + "<td>" + obj.lastname + "</td>"
+                    + "<td>" + obj.message + "</td>"
+                    + "<td><button type=\"button\" onclick=\"fillFields('" + obj.id + "\', '" + obj.firstname + "\', \'"
+                    + obj.lastname + "\', \'" + obj.message + "\')\" class=\"btn btn-light\">Update</button>"
+                    + " | <button type=\"button\" onclick=\"deleteDocument(\'" + obj.id + "\')\" class=\"btn btn-danger\">Delete</button></td>");
+            });
         },
         error: function(err) {
-            console.log("AJAX error in request: " + JSON.stringify(err, null, 2));
             alert(JSON.stringify(err, null, 2));
         }
     });
@@ -26,7 +23,7 @@ function fillTable(){
 
 function deleteDocument(id) {
     $.getJSON("/api/delete/", {id: id});
-    alert('The document was deleted.');
+    alert("The document was deleted.");
     $(fillTable());
 }
 
@@ -38,11 +35,11 @@ function fillFields(id, firstname, lastname, message) {
 }
 
 function clearFields() {
-    $("#docId").val('');
-    $("#firstName").val('');
-    $("#lastName").val('');
-    $("#message").val('');
-    $("#query").val('');
+    $("#docId").val("");
+    $("#firstName").val("");
+    $("#lastName").val("");
+    $("#message").val("");
+    $("#query").val("");
 }
 
 $(document).ready(function() {
@@ -50,26 +47,23 @@ $(document).ready(function() {
     $(fillTable());
 
     $( "#query" ).keyup(function() {
-        $.getJSON("/api/search/", {query: $('#query').val()} , function(result) {
-            $('#myTable').find("tr:gt(0)").remove();
-            for (i = 0; i <= result.length - 1; i++) {
-                var obj = result[i];
-                $('#myTable > tbody:last').after('<tr><td>' + obj.firstname + '</td>'
-                    + '<td>' + obj.lastname + '</td>'
-                    + '<td>' + obj.message + '</td>'
-                    + '<td><button type=\'button\' onclick=\"fillFields(\'' + obj.id + '\', \'' + obj.firstname + '\', \''
-                    + obj.lastname + '\', \'' + obj.message + '\')\" class=\'btn btn-light\'>Update</button>'
-                    + ' | <button type=\'button\' onclick=deleteDocument(\'' + obj.id + '\') class=\'btn btn-danger\'>Delete</button></td></tr>');
-            };
+        $.getJSON("/api/search/", {query: $("#query").val()} , function(result) {
+            $("#myTable").find("tr:gt(0)").remove();
+            result.forEach(function (obj) {
+                $("#myTable > tbody:last").after("<tr><td>" + obj.firstname + "</td>"
+                    + "<td>" + obj.lastname + "</td>"
+                    + "<td>" + obj.message + "</td>"
+                    + "<td><button type=\"button\" onclick=\"fillFields('" + obj.id + "\', '" + obj.firstname + "\', \'"
+                    + obj.lastname + "\', \'" + obj.message + "\')\" class=\"btn btn-light\">Update</button>"
+                    + " | <button type=\"button\" onclick=\"deleteDocument(\'" + obj.id + "\')\" class=\"btn btn-danger\">Delete</button></td></tr>");
+            });
         });
     });
 
     $("#save").click(function(){
         event.preventDefault();
-
-
-        var id = '' === $("#docId").val()?null:$("#docId").val();
-        var url = null === id?'/api/create':'/api/update';
+        var id = "" === $("#docId").val()?null:$("#docId").val();
+        var url = null === id?"/api/create":"/api/update";
         var document = {
             id: id,
             firstname: $("#firstName").val(),
@@ -78,7 +72,7 @@ $(document).ready(function() {
         };
 
         $.ajax({
-            type: 'POST',
+            type: "POST",
             url: url,
             data: JSON.stringify(document),
             contentType: "application/json; charset=utf-8",
@@ -87,7 +81,6 @@ $(document).ready(function() {
                 $(fillTable());
             },
             error: function(err) {
-                console.log("AJAX error in request: " + JSON.stringify(err, null, 2));
                 alert(JSON.stringify(err, null, 2));
             }
         });
